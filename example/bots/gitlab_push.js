fe758fbbec5ffecf11612c0703af9b77b3159cc8
zaru@sakuraba <zarutofu@gmail.com>
2014-06-14 17:47:04 +0900

    GitLabのマージリクエストとIssueのイベントにも対応

--- example/bots/gitlab_push.js
+++ example/bots/gitlab_push.js
@@ -2,6 +2,13 @@ module.exports = function (chabot) {
 
    // WebHook で受けたデータをセット
     var payload = chabot.data;
+    
+    // マージリクエストのクローズは受け取らない
+    if (typeof payload.object_kind !== 'undefined') {
+    	if (payload.object_kind == 'merge_request' && payload.object_attributes.state != 'opened') {
+    		return;
+    	}
+    }
  
     // ChatWork API の endpoint をセット
     var endpoint = '/rooms/' + chabot.roomid + '/messages';
@@ -9,6 +16,7 @@ module.exports = function (chabot) {
     var template = chabot.readTemplate('gitlab_push.ejs');
     // WebHook で受けたデータでメッセージテンプレートを描画
     var message_body = chabot.render(template, payload);
+    
 
     // ChatWork API でメッセージ送信
     chabot.client
